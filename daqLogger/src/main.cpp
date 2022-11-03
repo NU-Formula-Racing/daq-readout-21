ad7972e1dda16adea4f835190c5ded622839276a
srsavas42 <stephensavas2024@u.northwestern.edu>
2022-11-02 20:07:12 -0500

    send RTC data to SD

--- daqLogger/src/main.cpp
+++ daqLogger/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
 
-#include <RTClib.h>
+#include "RTClib.h"
 #include "virtualTimer.h"
 #include "can_interface.h"
-//#include "virtualTimer.h"
+#include "virtualTimer.h"
+
+
+RTC_PCF8523 rtc;
+
+// FAILURE MODES:
+//  Is data all changing? If not, sensor could be broken
+//  Is data not being sent from a specific sensor? If not, could be broken
+
+
 
 // Goals:
 //   Read HP CAN
@@ -29,9 +40,9 @@ TeensyCAN<1> can_bus{};
 #endif
 
 #ifdef ARDUINO_ARCH_ESP32
-//#include "esp_can.h"
+#include "esp_can.h"
 // The tx and rx pins are constructor arguments to ESPCan, which default to TX = 5, RX = 4
-//ESPCAN can_bus{};
+ESPCAN can_bus{};
 #endif
 // How do I differentiate HP vs LP???
 
@@ -50,13 +61,8 @@ TeensyCAN<1> can_bus{};
 //       timer %10 = 0, store info from dict[10]; 
 //       timer %100 = 0, store dict[100]; 
 //       timer %1000 = 0, store dict[1000];
-//    
-#include "SD.h"
-#include "SPI.h"
+//
 
-unsigned long previousTime10Analog = millis();
-unsigned long previousTime100Analog = millis();
-unsigned long previousTime1000Analog = millis();
 /*
 // pulls all analog values and compiles into CSV string
 void compileCurData(delta){
@@ -80,21 +86,103 @@ void compileCurData(delta){
 
 File sensorData;
 String fileName;
+String dataString ="";
+const int CSpin = 53;
+unsigned long previousTimeAnalog = millis();
+
+void saveData(){
+  // reopen file
+  sensorData = SD.open(fileName, FILE_WRITE);
+    if (sensorData){
+     // print line into csv
+      sensorData.println(dataString);
+      sensorData.close();
+      Serial.println(dataString);
+   } else {
+    Serial.println("Error saving values to file !");
+  }
+}
+
+void compileCurData(){
+  // convert to CSV
+  dataString = "";
+  for (int i = 1; i <= 4; i = i + 1) {
+    dataString = dataString + "Test "+ i + ",";
+  }
+  
+}
 
 void setup(void){
+  Serial.begin(57600);
+  Serial.print("Initializing RTC...");
+
+  #ifndef ESP8266
+    while (!Serial); // wait for serial port to connect. Needed for native USB
+  #endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.initialized() || rtc.lostPower()) {
+    Serial.println("RTC is NOT initialized, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  rtc.start();
+
+  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
+  int offset = round(deviation_ppm / drift_unit);
+  // rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
+  // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
+
+  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
+
   Serial.begin(9600);
   Serial.print("Initializing SD card...");
 
-  fileName = "test";
-  sensorData = SD.open(fileName, FILE_WRITE);
+  if (!SD.begin(CSpin)) {
+    Serial.println("Card failed/not found.");
+  } else{
+    Serial.print("Card Initialized.");
 
-  if (sensorData){
-    sensorData.println("FL_VSS,FR_VSS,BL_VSS,BR_VSS,FL_BRK_TMP,FR_BRK_TMP,BL_BRK_TMP,BR_BRK_TMP,FL_SUS_POT,FR_SUS_POT,BL_SUS_POT,BR_SUS_POT,F_BRK_PRES,B_BRK_PRES,STEER_ANG,TPS,OIL_PRES,OIL_TEMP,COOL_TEMP,MAP,MAT,NEUT,LAMBDA1,LAMBDA2,ACCELX,ACCELY,ACCELZ,STRAIN1,STRAIN2,STRAIN3,STRAIN4,PTUBE1,PTUBE2,PTUBE3,PTUBE4,PTUBE5,PTUBE6,PTUBE7,PTUBE8,PTUBE9,PTUBE10,PTUBE11,PTUBE12,GYROX,GYROY,GYROZ,MAGNETX,MAGNETY,MAGNETZ,DAQTEMP");
+    fileName = "test";
+    sensorData = SD.open(fileName, FILE_WRITE);
+
+    if (sensorData){
+      sensorData.println("Month, Day, Year, Hour, Minute, Second");
+      //sensorData.println("FL_VSS,FR_VSS,BL_VSS,BR_VSS,FL_BRK_TMP,FR_BRK_TMP,BL_BRK_TMP,BR_BRK_TMP,FL_SUS_POT,FR_SUS_POT,BL_SUS_POT,BR_SUS_POT,F_BRK_PRES,B_BRK_PRES,STEER_ANG,TPS,OIL_PRES,OIL_TEMP,COOL_TEMP,MAP,MAT,NEUT,LAMBDA1,LAMBDA2,ACCELX,ACCELY,ACCELZ,STRAIN1,STRAIN2,STRAIN3,STRAIN4,PTUBE1,PTUBE2,PTUBE3,PTUBE4,PTUBE5,PTUBE6,PTUBE7,PTUBE8,PTUBE9,PTUBE10,PTUBE11,PTUBE12,GYROX,GYROY,GYROZ,MAGNETX,MAGNETY,MAGNETZ,DAQTEMP");
+      sensorData.close();
+    }
+    saveData();
     sensorData.close();
   }
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+
+  DateTime now = rtc.now();
+
+  if (currentTime < 10000) {
+
+    // check for analog reading every second
+    if (currentTime - previousTimeAnalog > 1000) {
+      previousTimeAnalog = currentTime;
+      dataString = "";
+      dataString = dataString + now.month() + ","+ now.day() + "," + now.year() + ","+ now.hour() + "," + now.minute() + "," + now.second() + ",";
+      saveData();
+    }
+  }
+  
+  
+  
 
   //digitalWrite(10, LOW);
 
