e422ed368cc57e4a20e1373359679781508bbe88
Isabel Zhong <isabelzhong30@gmail.com>
2023-04-29 15:20:16 -0500

    fixed sd writing issues

--- daqLogger/src/main.cpp
+++ daqLogger/src/main.cpp
@@ -8,8 +8,10 @@
 #include "can_interface.h"
 #include "virtualTimer.h"
 #include "inverter_driver.h"
+#include <string>
 
 RTC_PCF8523 rtc;
+VirtualTimerGroup timer_group;
 
 // Initialize canbus based on teensy board
 #if defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
@@ -21,7 +23,7 @@ TeensyCAN<1> can_bus{};
 #ifdef ARDUINO_ARCH_ESP32
 #include "esp_can.h"
 // The tx and rx pins are constructor arguments to ESPCan, which default to TX = 5, RX = 4; Currenlty tx=32, rx=27
-ESPCAN can_bus{GPIO_NUM_32, GPIO_NUM_27};
+ESPCAN can_bus{10, GPIO_NUM_32, GPIO_NUM_27};
 #endif
 
 // LP CAN Addresses
@@ -34,59 +36,56 @@ const int kGPS_CAN = 0x430;
 const int kACCEL_CAN = 0x431;
 const int kGYRO_CAN = 0x432;
 
-
 // LP CAN Messages
 
 // Front left wheel speed and temp
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> FL_wheel_speed_signal{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> FL_wheel_speed_signal{};
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> FL_brake_temp_signal{};
 CANSignal<float, 32, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0.1), false> FL_suspension_position_signal{};
-CANRXMessage<3> rx_message_FLwheel{can_bus, kFL_CAN, FL_wheel_speed_signal, FL_brake_temp_signal, FL_suspension_position_signal};
+CANRXMessage<2> rx_message_FLwheel{can_bus, kFL_CAN, FL_wheel_speed_signal, FL_brake_temp_signal};
+CANTXMessage<2> tx_message_FLwheel{can_bus, kFL_CAN, 4, 100, timer_group, FL_wheel_speed_signal, FL_brake_temp_signal};
 
 // Front right wheel speed and temp
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> FR_wheel_speed_signal{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> FR_wheel_speed_signal{};
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> FR_brake_temp_signal{};
 CANSignal<float, 32, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0.1), false> FR_suspension_position_signal{};
 CANRXMessage<3> rx_message_FRwheel{can_bus, kFR_CAN, FR_wheel_speed_signal, FR_brake_temp_signal, FR_suspension_position_signal};
 
 // Back left wheel speed and temp
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> BL_wheel_speed_signal{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> BL_wheel_speed_signal{};
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> BL_brake_temp_signal{};
 CANSignal<float, 32, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0.1), false> BL_suspension_position_signal{};
 CANRXMessage<3> rx_message_BLwheel{can_bus, kBL_CAN, BL_wheel_speed_signal, BL_brake_temp_signal, BL_suspension_position_signal};
 
 // Back right wheel speed and temp
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> BR_wheel_speed_signal{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> BR_wheel_speed_signal{};
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> BR_brake_temp_signal{};
 CANSignal<float, 32, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0.1), false> BR_suspension_position_signal{};
 CANRXMessage<3> rx_message_BRwheel{can_bus, kBR_CAN, BR_wheel_speed_signal, BR_brake_temp_signal, BR_suspension_position_signal};
 
 // Brake Pressure
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0.1), true> F_Brake_pressure{}; 
-CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0.1), true> R_Brake_pressure{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0.1), true> F_Brake_pressure{};
+CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0.1), true> R_Brake_pressure{};
 CANRXMessage<2> rx_message_brake{can_bus, kBrake_CAN, F_Brake_pressure, R_Brake_pressure};
 
 // // Acceleration
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> accel_x{}; 
-CANSignal<float, 16, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> accel_y{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> accel_x{};
+CANSignal<float, 16, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> accel_y{};
 CANSignal<float, 32, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> accel_z{};
 CANRXMessage<3> rx_message_accel{can_bus, kACCEL_CAN, accel_x, accel_y, accel_z};
 
 // Gyro
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_x{}; 
-CANSignal<float, 16, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_y{}; 
-CANSignal<float, 32, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_z{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_x{};
+CANSignal<float, 16, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_y{};
+CANSignal<float, 32, 16, CANTemplateConvertFloat(0.0005), CANTemplateConvertFloat(0), false> gyro_z{};
 CANRXMessage<3> rx_message_gyro{can_bus, kGYRO_CAN, gyro_x, gyro_y, gyro_z};
 
 // GPS
-CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), true> lon_signal{}; 
-CANSignal<float, 32, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), true> lat_signal{}; 
+CANSignal<float, 0, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), true> lon_signal{};
+CANSignal<float, 32, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), true> lat_signal{};
 CANRXMessage<2> rx_message_gps{can_bus, kGPS_CAN, lon_signal, lat_signal};
 
 // // BMS Cell V Summary
-
-
-
 
 // HP CAN Addresses
 const int kTemp_CAN = 0x420;
@@ -101,11 +100,9 @@ CANRXMessage<2> rx_message_temp{can_bus, kTemp_CAN, coolant_temp_signal, ambient
 
 // BMS
 
-
 // Throttle
 CANSignal<uint8_t, 0, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> cur_throttle_signal{};
 CANRXMessage<1> rx_message_throttle{can_bus, kThrottle_CAN, cur_throttle_signal};
-
 
 const int CSpin = 5;
 const int new_SDA = 21;
@@ -113,67 +110,74 @@ const int new_SCL = 22;
 
 File sensorData;
 String fileName;
-String dataString ="";
-
-VirtualTimerGroup timer_group;
+String dataString = "";
 
 Inverter inverter{can_bus};
 
-void saveData(){
+void saveData()
+{
   // reopen file
-  sensorData = SD.open(fileName, FILE_WRITE);
-  if (sensorData){
+  sensorData = SD.open(fileName, FILE_APPEND);
+  if (sensorData)
+  {
     // print line into csv
     sensorData.println(dataString);
-    sensorData.close();
     Serial.println(dataString);
-  } else {
+    sensorData.close();
+  }
+  else
+  {
     Serial.println("Error saving values to file !");
   }
 }
 
-
 // Make these iteratively
-void sensor10ms(){
+void sensor10ms()
+{
   DateTime now = rtc.now();
   // dataString = dataString + "\n" + now.timestamp();
   // dataString = dataString + "\n" + now.timestamp() + ", , , , , , , , " + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", , , , , ";
   saveData();
 }
 
-void sensor100ms(){
+void sensor100ms()
+{
   DateTime now = rtc.now();
 
   // dataString = dataString + "\n" + now.timestamp() + ", , , , , , , , , ," + inverter.GetRPM() + ", " + inverter.GetInverterTemperature() + ", " + inverter.GetMotorTemperature() + "," + float(coolant_temp_signal) + "," + float(ambient_temp_signal);// + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", " + float(gyro_x) + ", " + float(gyro_y) + ", " + float(gyro_z);//float(FL_wheel_speed_signal) + ", " + float(FR_wheel_speed_signal);
 
-  //dataString = dataString + "\n" + now.timestamp() + float(FL_wheel_speed_signal) + float(FR_wheel_speed_signal) + float(BL_wheel_speed_signal) + float(BR_wheel_speed_signal) + ", , , , , " + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", " + float(gyro_x) + ", " + float(gyro_y) + ", " + float(gyro_z) + ", " + float(lon_signal) + ", " + float(lat_signal);
+  // dataString = dataString + "\n" + now.timestamp() + float(FL_wheel_speed_signal) + float(FR_wheel_speed_signal) + float(BL_wheel_speed_signal) + float(BR_wheel_speed_signal) + ", , , , , " + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", " + float(gyro_x) + ", " + float(gyro_y) + ", " + float(gyro_z) + ", " + float(lon_signal) + ", " + float(lat_signal);
   saveData();
 }
 
-void sensor1000ms(){
+void sensor1000ms()
+{
   DateTime now = rtc.now();
-  Serial.print("X Accel");
-  Serial.print(accel_x, 4);
-
+  String placeholder = "";
   //                                Timestamp                        FL Speed                             FR Speed                               BL Speed                             BR Speed                             FL Brake Temp                        FR Brake Temp                        BL Brake Temp                       BR Brake Temp                     F Brake Pressure                 B Brake Pressure                     RPM                         Inverter Temp                              Motor Temp                             Coolant Temp                      Ambient Temp                    Accel_x                 Accel_y                Accel_z                  Gyro_x                Gyro_y                  Gyro_z                  Longitude                   Latitude
-  dataString = dataString + "\n" + now.timestamp() + ", " + float(FL_wheel_speed_signal) + ", " + float(FR_wheel_speed_signal) + ", " + float(BL_wheel_speed_signal) + ", " + float(BR_wheel_speed_signal) + ", " + float(FL_brake_temp_signal) + ", " + float(FR_brake_temp_signal) + ", " + float(BL_brake_temp_signal) + ", " + float(BR_brake_temp_signal) + ", " + float(F_Brake_pressure) + ", " + float(R_Brake_pressure) + ", " + inverter.GetRPM() + ", " + inverter.GetInverterTemperature() + ", " + inverter.GetMotorTemperature() + "," + float(coolant_temp_signal) + "," + float(ambient_temp_signal) + ", " + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", " + float(gyro_x) + ", " + float(gyro_y) + ", " + float(gyro_z) + ", " + float(lon_signal) + ", " + float(lat_signal);
+  dataString = placeholder + now.timestamp() + ", " + float(FL_wheel_speed_signal) + ", " + float(FR_wheel_speed_signal) + ", " + float(BL_wheel_speed_signal) + ", " + float(BR_wheel_speed_signal) + ", " + float(FL_brake_temp_signal) + ", " + float(FR_brake_temp_signal) + ", " + float(BL_brake_temp_signal) + ", " + float(BR_brake_temp_signal) + ", " + float(F_Brake_pressure) + ", " + float(R_Brake_pressure) + ", " + inverter.GetRPM() + ", " + inverter.GetInverterTemperature() + ", " + inverter.GetMotorTemperature() + "," + float(coolant_temp_signal) + "," + float(ambient_temp_signal) + ", " + float(accel_x) + ", " + float(accel_y) + ", " + float(accel_z) + ", " + float(gyro_x) + ", " + float(gyro_y) + ", " + float(gyro_z) + ", " + float(lon_signal) + ", " + float(lat_signal);
   saveData();
 }
 
-DateTime init_RTC(){
-  #ifndef ESP8266
-    while (!Serial); // wait for serial port to connect. Needed for native USB
-  #endif
+DateTime init_RTC()
+{
+#ifndef ESP8266
+  while (!Serial)
+    ; // wait for serial port to connect. Needed for native USB
+#endif
 
-  Wire.begin(new_SDA,new_SCL);
+  Wire.begin(new_SDA, new_SCL);
 
-  if (! rtc.begin()) {
+  if (!rtc.begin())
+  {
     Serial.println("Couldn't find RTC");
     Serial.flush();
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
 
-  if (! rtc.initialized() || rtc.lostPower()) {
+  if (!rtc.initialized() || rtc.lostPower())
+  {
     Serial.println("RTC is NOT initialized, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
@@ -182,43 +186,42 @@ DateTime init_RTC(){
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   DateTime now = rtc.now();
 
-  float drift = -10; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
-  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float drift = -10;                                    // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);                       // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
   float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
-  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  float drift_unit = 4.34;                              // use with offset mode PCF8523_TwoHours
   // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
   int offset = round(deviation_ppm / drift_unit);
   rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
   // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
 
-  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
+  Serial.print("Offset is ");
+  Serial.println(offset); // Print to control offset
 
   return now;
 }
 
-void init_SD(DateTime now){
-  if (!SD.begin(CSpin)) {
+void init_SD(DateTime now)
+{
+  if (!SD.begin(CSpin))
+  {
     Serial.println("Card failed/not found.");
-  } else{
+  }
+  else
+  {
     Serial.print("Card Initialized.");
 
     // New file name not working yet, does work with /test.txt
     fileName = "/test-" + String(now.month()) + "-" + String(now.day()) + "=" + String(now.hour()) + "-" + String(now.minute()) + ".csv";
-
     sensorData = SD.open(fileName, FILE_WRITE);
-
-    if (sensorData){
-      // LP Headers:
-      dataString = "Timestamp, FL Speed, FR Speed, BL Speed, BR Speed, FL Brake Temp, FR Brake Temp, BL Brake Temp, BR Brake Temp, F Brake Pressure, B Brake Pressure, RPM, Inverter Temp, Motor Temp, Coolant Temp, Ambient Temp, Accel_x, Accel_y, Accel_z, Gyro_x, Gyro_y, Gyro_z, Longitude, Latitude";
-      // HP Headers:
-      saveData();
-    }
-
-    sensorData.close();
+    dataString = "Timestamp, FL Speed, FR Speed, BL Speed, BR Speed, FL Brake Temp, FR Brake Temp, BL Brake Temp, BR Brake Temp, F Brake Pressure, B Brake Pressure, RPM, Inverter Temp, Motor Temp, Coolant Temp, Ambient Temp, Accel_x, Accel_y, Accel_z, Gyro_x, Gyro_y, Gyro_z, Longitude, Latitude";
+    //  HP Headers:
+    saveData();
   }
 }
 
-void setup(void){
+void setup(void)
+{
   Serial.begin(9600);
 
   pinMode(GPIO_NUM_26, OUTPUT);
@@ -228,16 +231,16 @@ void setup(void){
 
   Serial.print("Initializing CAN...");
   can_bus.Initialize(ICAN::BaudRate::kBaud1M);
-  
-  can_bus.RegisterRXMessage(rx_message_FLwheel);
-  can_bus.RegisterRXMessage(rx_message_FRwheel);
-  can_bus.RegisterRXMessage(rx_message_BLwheel);
-  can_bus.RegisterRXMessage(rx_message_BRwheel);
-  can_bus.RegisterRXMessage(rx_message_brake);
-  can_bus.RegisterRXMessage(rx_message_accel);
-  can_bus.RegisterRXMessage(rx_message_gyro);
-  can_bus.RegisterRXMessage(rx_message_gps);
-  can_bus.RegisterRXMessage(rx_message_temp);
+
+  // can_bus.RegisterRXMessage(rx_message_FLwheel);
+  // can_bus.RegisterRXMessage(rx_message_FRwheel);
+  // can_bus.RegisterRXMessage(rx_message_BLwheel);
+  // can_bus.RegisterRXMessage(rx_message_BRwheel);
+  // can_bus.RegisterRXMessage(rx_message_brake);
+  // can_bus.RegisterRXMessage(rx_message_accel);
+  // can_bus.RegisterRXMessage(rx_message_gyro);
+  // can_bus.RegisterRXMessage(rx_message_gps);
+  // can_bus.RegisterRXMessage(rx_message_temp);
   Serial.print("CAN Initialized");
 
   Serial.print("Initializing Inverter...");
@@ -245,12 +248,6 @@ void setup(void){
   inverter.RequestPowerStageTemp(100);
   inverter.RequestRPM(100);
   Serial.print("Inverter Initialized");
-
-  Serial.print("Initializing Timers...");
-  timer_group.AddTimer(10U, sensor10ms);
-  timer_group.AddTimer(100U, sensor100ms);
-  timer_group.AddTimer(1000U, sensor1000ms);
-  Serial.print("Timers Initialized");
 
   Serial.print("Initializing RTC...");
   DateTime now = init_RTC();
@@ -260,14 +257,15 @@ void setup(void){
   init_SD(now);
   Serial.print("SD Card Initialized");
 
+  Serial.print("Initializing Timers...");
+  // timer_group.AddTimer(10U, sensor10ms);
+  // timer_group.AddTimer(100U, sensor100ms);
+  timer_group.AddTimer(1000, sensor1000ms);
+  Serial.print("Timers Initialized");
 }
 
-void loop() {
-
+void loop()
+{
   can_bus.Tick();
   timer_group.Tick(millis());
-
 }
-
-
-
